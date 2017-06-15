064b730b7f5e142c6eedb4174932cc5175809ea4
Alejandro De La Cruz Sunza <16470358@itcampeche.edu.mx>
2017-06-14 23:10:39 -0500

    El Proyecto implementa las clases CuentaDeAhorro y MainCompleto.

--- src/Modelo/CuentaBasica.java
+++ src/Modelo/CuentaBasica.java
@@ -11,4 +11,53 @@ package Modelo;
  */
 public class CuentaBasica {
     
+    private int NumeroDeCuenta; 
+    private String Cliente = null;
+    private double Saldo;
+    private double Cantidad;
+    private double Retiro;
+
+
+    
+    
+    public CuentaBasica (int NumeroDeCuenta, String Cliente,int Saldo)
+    {
+        this.NumeroDeCuenta = 1;
+        this.Cliente = " ALEX SUNZA ";
+        this.Saldo = Saldo = 0;
+    }
+
+   
+
+    public double getSaldo()
+    {
+        return this.Saldo;
+    }
+public double depositar(double Deposito)
+    {
+        this.Cantidad = Deposito;
+        Saldo = Saldo + Cantidad;
+        return Saldo;
+    }
+        public boolean retirar (double Retirar)
+    {
+        boolean xSaldo=true;
+        this.Retiro = Retirar;
+        if(Retiro <= Saldo)
+        {
+            Saldo = Saldo - Retiro;
+            xSaldo=true;
+        }
+        else
+        {
+            if (Retiro > Saldo)
+            {
+                xSaldo=false;
+            }
+ 
+        }
+        return xSaldo;
+
+    }
+    
 }
